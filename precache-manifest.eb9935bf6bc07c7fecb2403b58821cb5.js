f56ff2ec4c2b9820437a011a2b2856529656710f
Brandyn Kusenda <bkusenda@gmail.com>
2021-09-22 12:14:31 -0700

    first push

---
+++ precache-manifest.eb9935bf6bc07c7fecb2403b58821cb5.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96cd063677e4dabf15833b3abcdc7a05",
+    "url": "/backgrounds/test.svg"
+  },
+  {
+    "revision": "80912b1dcc93ff486c9eb365c904a215",
+    "url": "/backgrounds/tree_faded.svg"
+  },
+  {
+    "revision": "a912dd979b4c96bcc3cc",
+    "url": "/css/app.6ebc1b93.css"
+  },
+  {
+    "revision": "d95ea820aa64e6679280",
+    "url": "/css/chunk-vendors.5161535d.css"
+  },
+  {
+    "revision": "d618f24afac6acaa10d81ac5e0f5b05f",
+    "url": "/img/brandyn_kusenda_bw.jpg"
+  },
+  {
+    "revision": "023157ea9f4f150b85e32a6f357d7b4a",
+    "url": "/img/pistar_edit_w.023157ea.png"
+  },
+  {
+    "revision": "595fdba04c917c26911ecd5196ab6ffc",
+    "url": "/img/pistarlab_agent_training1.595fdba0.png"
+  },
+  {
+    "revision": "a0c02be98e31a6e23315fb74b1bca3d7",
+    "url": "/img/twitter.svg"
+  },
+  {
+    "revision": "73b67d02c8fde8eade0e7bae66f382d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "742e3ea109d225abae05",
+    "url": "/js/about.cfa4bddd.js"
+  },
+  {
+    "revision": "a912dd979b4c96bcc3cc",
+    "url": "/js/app.16ec5e72.js"
+  },
+  {
+    "revision": "d95ea820aa64e6679280",
+    "url": "/js/chunk-vendors.fb0a237e.js"
+  },
+  {
+    "revision": "9f9fa474f6b63651b5e447b7d220570e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "e13b6e2c1b5807d9b26eb3da67fe10ca",
+    "url": "/splash.jpg"
+  }
+]);
